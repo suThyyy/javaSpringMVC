4c4f46be1673905c36aa17694f604d24445b4d35
suThyy <oqnvuz0704@gmail.com>
2025-02-23 15:57:51 +0700

    #58

--- src/main/java/vn/hoidanit/laptopshop/domain/User.java
+++ src/main/java/vn/hoidanit/laptopshop/domain/User.java
@@ -1,7 +1,14 @@
 package vn.hoidanit.laptopshop.domain;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "nguoi_dung")
 public class User {
 
+    @Id
     private long id;
     private String email, password, fullName, address, phone;
 
