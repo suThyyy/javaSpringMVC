eddfcef16d50b88418d60a21f80e84ff6e74b951
suThyy <oqnvuz0704@gmail.com>
2025-02-28 19:40:45 +0700

    #76

--- src/main/java/vn/hoidanit/laptopshop/domain/User.java
+++ src/main/java/vn/hoidanit/laptopshop/domain/User.java
@@ -19,6 +19,8 @@ public class User {
     private String address;
     private String phone;
 
+    private String avatar;
+
     public long getId() {
         return id;
     }
@@ -67,10 +69,18 @@ public class User {
         this.phone = phone;
     }
 
+    public String getAvatar() {
+        return avatar;
+    }
+
+    public void setAvatar(String avatar) {
+        this.avatar = avatar;
+    }
+
     @Override
     public String toString() {
         return "User [id=" + id + ", email=" + email + ", password=" + password + ", fullName=" + fullName
-                + ", address=" + address + ", phone=" + phone + "]";
+                + ", address=" + address + ", phone=" + phone + ", avatar=" + avatar + "]";
     }
 
 }
